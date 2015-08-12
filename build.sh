d62d0d68fa65d71270ae02962b0d12ddeef483a9
dSeidel <d.seidel@alkacon.com>
2015-08-12 09:03:34 +0200

    removed files.

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/user/git/git-conflicts-v1/modules/org.opencms.test.reducedexport/
-zip -r org.opencms.test.reducedexport.zip ./*
-mv org.opencms.test.reducedexport.zip /home/user/dev/apache-tomcat-8/webapps/oc10/WEB-INF/packages/modules/
--- fetch-export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/user/git/git-conflicts-v1/modules/org.opencms.test.reducedexport/
-mv /home/user/dev/apache-tomcat-8/webapps/oc10/WEB-INF/packages/modules/org.opencms.test.reducedexport_1.0.zip ./org.opencms.test.reducedexport.zip
-unzip -o org.opencms.test.reducedexport.zip
-rm org.opencms.test.reducedexport.zip
